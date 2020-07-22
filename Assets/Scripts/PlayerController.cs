cd1189b71a3ca6bde1bca369112603dfab706dd0
Matthew Bickerton <>
2020-07-21 20:05:22 -0400

    walking animations, changed lighting to linear, probably some other minor stuff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,19 +6,35 @@ public class PlayerController : MonoBehaviour {
     public float speed;
 
     new private Rigidbody2D rigidbody2D;
+    private Animator animator;
+    private float currentDirection;
 
     // Use this for initialization
     void Start()
     {
         //Get and store a reference to the Rigidbody2D component so that we can access it.
-        rigidbody2D = GetComponent<Rigidbody2D> ();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        if (transform.rotation.y == 0) currentDirection = 1f; else currentDirection = -1f;
     }
 
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
     void FixedUpdate()
     {
         //Store the current horizontal input in the float moveHorizontal.
-        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+
+        if (moveHorizontal != 0) {
+            animator.SetBool("Walk", true);
+        }
+        else {
+            animator.SetBool("Walk", false);
+        }
+
+        if (currentDirection != moveHorizontal && moveHorizontal != 0) {
+            currentDirection = moveHorizontal;
+            transform.Rotate(0f, 180f, 0f, Space.Self);
+        }
 
         //Use the two store floats to create a new Vector2 variable movement.
         Vector2 movement = new Vector2 (moveHorizontal * speed, 0);
