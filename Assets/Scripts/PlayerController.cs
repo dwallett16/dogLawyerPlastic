83e36a3b1087d6d421562ea3ccf5c8ffb6eab9ad
Matthew Bickerton <>
2020-07-22 17:50:56 -0400

    added new spine animation controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,42 +8,123 @@ public class PlayerController : MonoBehaviour {
     new private Rigidbody2D rigidbody2D;
     private Animator animator;
     private float currentDirection;
+    
+    public SpineAnimatorController spineAnimatorController;
+    public enum PlayerState {
+        Idle,
+        Walk,
+        SmokeStart,
+        Smoking,
+        SmokeEnd
+    }
+
+    private PlayerState currentState, previousState;
+    bool stateChanged;
+    float moveHorizontal = 0f;
 
     // Use this for initialization
     void Start()
     {
         //Get and store a reference to the Rigidbody2D component so that we can access it.
         rigidbody2D = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        //animator = GetComponent<Animator>();
         if (transform.rotation.y == 0) currentDirection = 1f; else currentDirection = -1f;
+        spineAnimatorController = GetComponent<SpineAnimatorController>();
+        currentState = PlayerState.Idle;
+        previousState = currentState;
+        spineAnimatorController.PlayNewAnimation("Idle", true);
     }
 
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
     void FixedUpdate()
     {
         //Store the current horizontal input in the float moveHorizontal.
-        float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+        if (currentState == PlayerState.Idle || currentState == PlayerState.Walk) {
+            moveHorizontal = Input.GetAxisRaw ("Horizontal");
 
+            //Movement
+            Vector2 movement = new Vector2 (moveHorizontal * speed, 0);
+            rigidbody2D.velocity = movement;
+
+            //Flip image
+            if (currentDirection != moveHorizontal && moveHorizontal != 0) {
+                currentDirection = moveHorizontal;
+                transform.Rotate(0f, 180f, 0f, Space.Self);
+            }
+        }
+
+        //Determine next state
         if (moveHorizontal != 0) {
-            animator.SetBool("Walk", true);
+            currentState = PlayerState.Walk;
         }
         else {
-            animator.SetBool("Walk", false);
+            if (Input.GetButtonDown("Smoke")) {
+                currentState = PlayerState.SmokeStart;
+            }
+            else  if (!IsSmoking()) {
+                currentState = PlayerState.Idle;
+            }
         }
 
-        if (currentDirection != moveHorizontal && moveHorizontal != 0) {
-            currentDirection = moveHorizontal;
-            transform.Rotate(0f, 180f, 0f, Space.Self);
+        if (IsSmoking()) {
+            if (currentState == PlayerState.SmokeStart) {
+                if (spineAnimatorController.IsAnimationComplete()) {
+                    currentState = PlayerState.Smoking;
+                }
+            }
+            else if (currentState == PlayerState.Smoking) {
+                if (spineAnimatorController.IsAnimationComplete() && !Input.GetButton("Smoke")) {
+                    currentState = PlayerState.SmokeEnd;
+                }
+            }
+            else if (currentState == PlayerState.SmokeEnd) {
+                if (spineAnimatorController.IsAnimationComplete()) {
+                    currentState = PlayerState.Idle;
+                }
+            }
         }
 
-        if (moveHorizontal == 0) {
-            if (Input.GetButton("Smoke")) animator.SetBool("IsSmoking", true); else animator.SetBool("IsSmoking", false);
+        stateChanged = previousState != currentState;
+        previousState = currentState;
+        if (stateChanged) HandleStateChange();
+    }
+
+    void HandleStateChange() {
+        string stateName = null;
+        bool loop = false;
+        switch (currentState) {
+            case PlayerState.Idle:
+                stateName = "Idle";
+                loop = true;
+                break;
+            case PlayerState.Walk:
+                stateName = "Walk";
+                loop = true;
+                break;
+            case PlayerState.SmokeStart:
+                stateName = "SmokeStart";
+                loop = false;
+                break;
+            case PlayerState.Smoking:
+                stateName = "Smoking";
+                loop = true;
+                break;
+            case PlayerState.SmokeEnd:
+                stateName = "SmokeEnd";
+                loop = false;
+                break;
+            default:
+                break;
         }
 
-        //Use the two store floats to create a new Vector2 variable movement.
-        Vector2 movement = new Vector2 (moveHorizontal * speed, 0);
+        spineAnimatorController.PlayNewAnimation(stateName, loop);
+    }
 
-        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
-        rigidbody2D.velocity = movement;
+    private bool IsSmoking() {
+        if (currentState == PlayerState.SmokeStart || currentState == PlayerState.Smoking
+        || currentState == PlayerState.SmokeEnd) {
+            return true;
+        }
+        return false;
     }
 }
---
+++ Assets/Scripts/SpineAnimatorController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine;
+using Spine.Unity;
+
+public class SpineAnimatorController : MonoBehaviour
+{
+    public SkeletonAnimation skeletonAnimation;
+    public List<string> listOfAllAnimations;
+    private Spine.AnimationState animationState;
+    private bool isAnimationComplete;
+    private bool isAnimationStarted;
+    private bool isAnimationEnded;
+
+
+    void Start() {
+        //skeletonAnimation = GetComponent<SkeletonAnimation>();
+        //animationState = skeletonAnimation.AnimationState;
+        skeletonAnimation.AnimationState.Complete += delegate (TrackEntry trackEntry) {
+            Debug.Log("Animation complete");
+            isAnimationComplete = true;
+            isAnimationStarted = false;
+        };
+        skeletonAnimation.AnimationState.Start += delegate (TrackEntry trackEntry) {
+            Debug.Log("Animation started");
+            isAnimationComplete = false;
+            isAnimationStarted = true;
+        };
+        skeletonAnimation.AnimationState.End += delegate (TrackEntry trackEntry) {
+            Debug.Log("Animation ended");
+        };
+
+        //capitalize animation names
+        for(int i = 0; i < listOfAllAnimations.Count; i++) {
+            if (listOfAllAnimations[i].Length == 1) {
+                listOfAllAnimations[i] = char.ToUpper(listOfAllAnimations[i][0]) + listOfAllAnimations[i].Substring(1);
+            }
+        }
+    }
+
+    public void OnSpineAnimationStart(TrackEntry trackEntry) {
+        isAnimationStarted = true;
+        isAnimationComplete = false;
+    }
+
+    public void OnSpineAnimationComplete(TrackEntry trackEntry) {
+        isAnimationComplete = true;
+        isAnimationStarted = false;
+    }
+
+    public void OnSpineAnimationEnd(TrackEntry trackEntry) {
+        isAnimationEnded = true;
+    }
+
+    
+    public bool IsAnimationComplete() {
+        return isAnimationComplete;
+    }
+
+    public bool IsAnimationStarted() {
+        return isAnimationStarted;
+    }
+
+    public bool IsAnimationEnded() {
+        return isAnimationEnded;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayNewAnimation(string animation, bool loop) {
+        if (listOfAllAnimations.Contains(animation)) {
+            skeletonAnimation.AnimationState.SetAnimation(0, animation, loop);
+        }
+    }
+}
