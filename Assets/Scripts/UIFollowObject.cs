0bc93e1e134d1e044b3179c3ffc127b75f50b06b
Matthew Bickerton <>
2020-07-23 11:34:16 -0400

    restored dialogue to player, dialogue bubbles are now screen space instead of world space

---
+++ Assets/Scripts/UIFollowObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class UIFollowObject : MonoBehaviour
+{
+    public Canvas canvas;
+    public RectTransform UI;
+    public Transform follow;
+    public Vector2 offset;
+
+    private void Update ()
+    {
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(follow.position);
+        Vector2 canvasPos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.GetComponent<RectTransform>(), screenPos, null, out canvasPos);
+        UI.anchoredPosition = canvasPos + offset;
+    }
+}
