72cc9497bffc31ce4793915f0350454579225886
David Wallett <>
2020-07-27 14:50:04 -0400

    inventory obj

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public List<Evidence> EvidenceList { 
+        get 
+        {
+            return evidenceList;
+        }
+    }
+    [SerializeField]
+    private List<Evidence> evidenceList;
+
+    public List<Skill> SkillsList
+    {
+        get 
+        {
+            return skillsList;
+        }
+    }
+    [SerializeField]
+    private List<Skill> skillsList;
+
+    public void AddEvidence(Evidence evidence) 
+    {
+        if(evidenceList.TrueForAll(e => e.Id != evidence.Id))
+            evidenceList.Add(evidence);
+    }
+
+    public void AddSkill(Skill skill)
+    {
+        skillsList.Add(skill);
+    }
+}
