da092e324bcfcb8e7aeec5ef7e1ae60c508c3483
David Wallett <>
2020-07-27 23:10:59 -0400

    journal page 1

---
+++ Assets/Scripts/ButtonAlphaToggle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonAlphaToggle : MonoBehaviour
+{
+    private Image Background;
+    private Text ButtonText;
+
+    void Start() 
+    {
+        Background = gameObject.GetComponent<Image>();
+        ButtonText = gameObject.GetComponentInChildren<Text>();
+    }
+
+    void Update ()
+    {
+        var active = gameObject == EventSystem.current.currentSelectedGameObject;
+        Background.enabled = active;
+        ButtonText.color = active ? Color.white : Color.black;
+    }
+}
