e29484c00fe9d36bb2a5ac1f59cf603b65460f2f
David Wallett <>
2020-07-28 23:00:41 -0400

    toggle journal

---
+++ Assets/Scripts/UI/JournalController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class JournalController : MonoBehaviour
+{
+    public Canvas HomeCanvas, CaseEvidenceCanvas;//, EvidenceCanvas, PartyCanvas, CaseDaCanvas, DaCanvas, ControlsCanvas;
+    public GameObject Background, JournalPanel;
+    public GameObject Player;
+    private PlayerController PlayerController;
+    private Rigidbody2D PlayerBody;
+    private List<Canvas> CanvasList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerController = Player.GetComponent<PlayerController>();
+        PlayerBody = Player.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Journal")) 
+        {
+            ToggleJournal();
+        }
+    }
+
+    private void ToggleJournal() 
+    {
+        Background.SetActive(!Background.activeInHierarchy);
+        JournalPanel.SetActive(!JournalPanel.activeInHierarchy);
+        CaseEvidenceCanvas.enabled = !CaseEvidenceCanvas.enabled;
+        PlayerBody.velocity = Vector2.zero;
+        PlayerController.enabled = !PlayerController.enabled;
+    }
+}
