d645e430878078f37b9e8608b1948ba2d7d32162
Matthew Bickerton <>
2020-08-15 17:58:49 -0400

    -main room of ornery ostrich -imported ornery ostrich assets -created materials for all ornery ostrich objects -created normal maps for ornery ostrich materials

---
+++ Assets/Scripts/Animation/BarLightParameters.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarLightParameters : MonoBehaviour
+{
+    public float CycleOffset = 0f;
+    public float SpeedMultiplier = 1f;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetFloat("CycleOffset", CycleOffset);
+        animator.SetFloat("SpeedMultiplier", SpeedMultiplier);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
