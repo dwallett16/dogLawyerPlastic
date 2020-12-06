99bcb5bacd2ff54c94f1f2e5879b7b902fe8330b
Matthew Bickerton <>
2020-12-06 12:25:59 -0500

    smoking collider on objects

---
+++ Assets/Scripts/Environment/SmokeCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PixelCrushers.DialogueSystem;
+
+public class SmokeCollision : MonoBehaviour
+{
+    public Transform Actor;
+    public Transform Conversant;
+    public string Conversation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnParticleCollision(GameObject other) {
+        Debug.Log("particle collision!");
+         DialogueManager.StartConversation(Conversation, Actor, Conversant);
+    }
+}
