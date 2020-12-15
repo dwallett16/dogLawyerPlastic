6a0bc18599fab7a712b7fc58a8b41c319e089755
David Wallett <>
2020-12-14 20:01:55 -0500

    car for map transition

---
+++ Assets/Scripts/Animation/FloatBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class FloatBehavior : MonoBehaviour
+{
+    float originalY;
+
+    public float FloatStrength = 1;
+    public float Frequency = 1;
+
+    void Start()
+    {
+        this.originalY = this.transform.position.y;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x,
+            originalY + ((float)Math.Sin((Time.time * Math.PI * Frequency)) * FloatStrength),
+            transform.position.z);
+    }
+}
---
+++ Assets/Scripts/Map/CarController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public float SpeedTimer = 4.0f;
+    public float Speed = 20;
+    private bool finished;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(finished)
+            return;
+        SpeedTimer -= Time.deltaTime;
+        if(SpeedTimer <= 0) {
+            this.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(Speed, 0, 0);
+            finished = true;
+        }
+    }
+}
