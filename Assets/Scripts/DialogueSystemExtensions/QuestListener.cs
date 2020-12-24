63daad0f70a2f91d98c4ac169cde2be9cc1379c3
David Wallett <>
2020-12-24 11:31:25 -0500

    quest listener on dialogue manager to detect changes

---
+++ Assets/Scripts/DialogueSystemExtensions/QuestListener.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using PixelCrushers.DialogueSystem;
+using UnityEngine;
+
+public class QuestListener : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnQuestStateChange(string questName) 
+    {
+        GameDataSingleton.gameData.caseData.PopulateCases();
+    }
+}
