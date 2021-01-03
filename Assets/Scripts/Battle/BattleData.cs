d93c7c33e6f67fca15fbc47b3eb2b6ae76e188dd
David Wallett <>
2021-01-03 12:19:16 -0500

    renamed scriptable typo

---
+++ Assets/Scripts/Battle/BattleData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleData : MonoBehaviour
+{
+
+    private string displayName {get; set;}
+    private CharacterType type {get; set;}
+    private int stressCapacity {get; set;}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MapFromData(Character characterData)
+    {
+        displayName = characterData.Name;
+        type = characterData.Type;
+        stressCapacity = characterData.StressCapacity;
+    }
+}
