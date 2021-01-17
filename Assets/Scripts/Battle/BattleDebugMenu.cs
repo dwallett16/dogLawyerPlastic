b6fe4dea35ddaa4c47d694ddad34124c5261e0f1
David Wallett <>
2021-01-17 12:25:07 -0500

    debug object

---
+++ Assets/Scripts/Battle/BattleDebugMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleDebugMenu : MonoBehaviour
+{
+    public bool UseTestData;
+    public Case CaseData;
+    public List<Character> TotalParty;
+    public List<Character> StartingParty;
+    public List<Evidence> EvidenceList;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(StartingParty.Count > 2 || EvidenceList.Count > 3) {
+            throw new System.Exception("Incorrect debug list sizes");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
