6d0888a08dc6330ab4cd24ef6f114446cbb6fd97
David Wallett <>
2021-01-17 14:10:28 -0500

    mapping characters

--- Assets/Scripts/Battle/BattleController.cs
+++ Assets/Scripts/Battle/BattleController.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class BattleController : MonoBehaviour
 {
+    public BattleDebugMenu DebugMenu;
+    public List<GameObject> ProsecutionPlaceholders;
+    public List<GameObject> DefensePlaceholders;
+    public GameObject GenericCombatant;
+    private List<GameObject> prosecutors;
+    private List<GameObject> defenseAttorneys;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        prosecutors = new List<GameObject>();
+        defenseAttorneys = new List<GameObject>();
+        if(DebugMenu.UseTestData) {
+            Debug.Log("using test data");
+            InstantiateCombatants();
+            MapFromScriptableObject();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void InstantiateCombatants() {
+        foreach(var prosecutorPlaceholder in ProsecutionPlaceholders) {
+            var prosecutorInstance = Instantiate(GenericCombatant, prosecutorPlaceholder.transform);
+            prosecutors.Add(prosecutorInstance);
+        }
+        foreach(var defensePlaceholder in defenseAttorneys) {
+            var defenseAttorneyInstance = Instantiate(GenericCombatant, defensePlaceholder.transform);
+            defenseAttorneys.Add(defenseAttorneyInstance);
+        }
+    }
+
+    private void MapFromScriptableObject()
+    {
+        foreach(var partyMember in DebugMenu.StartingParty) {
+
+        }
     }
 }
--- Assets/Scripts/Battle/CharacterBattleData.cs
+++ Assets/Scripts/Battle/CharacterBattleData.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BattleData : MonoBehaviour
+public class CharacterBattleData : MonoBehaviour
 {
-    
+
+    private string displayName {get; set;}
+    private CharacterType type {get; set;}
+    private int stressCapacity {get; set;}
+
     // Start is called before the first frame update
     void Start()
     {
