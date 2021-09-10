ef0a9cd30e2365eb126cd998f3f78833b77d04c8
David Wallett <>
2021-09-09 22:03:51 -0400

    button hovering color change

--- Assets/Scripts/UI/GuildMenu/GuildItemToggle.cs
+++ Assets/Scripts/UI/GuildMenu/GuildItemToggle.cs
@@ -9,40 +9,17 @@ public class GuildItemToggle : MonoBehaviour
     private Image Background;
     private Text[] ButtonLabels;
 
-    private Button test;
-
     void Start() 
     {
-        Background = gameObject.GetComponent<Image>();
         ButtonLabels = gameObject.GetComponentsInChildren<Text>();
-        Background.enabled = false;
-        test = GetComponent<Button>();
-        //test.onClick.AddListener(log);
     }
 
     void Update ()
     {
         var active = gameObject == EventSystem.current.currentSelectedGameObject;
-        Background.enabled = active;
         for(var i = 0; i < ButtonLabels.Length; i++) {
             ButtonLabels[i].color= active ? Color.white : Color.black;
         }
-        //if(active)Debug.Log(GetComponent<RectTransform>().localPosition);
     }
 
-    public void log()
-    {
-        if(EventSystem.current.currentSelectedGameObject == gameObject)
-        {
-            Debug.Log("DOUBLE CLICK");
-        }
-    }
-
-    //public void OnPointerClick(PointerEventData eventData)
-    //{
-    //    if (eventData.clickCount == 1)
-    //        Debug.Log("SINGLE CLICK");
-    //    else if (eventData.clickCount >= 2)
-    //        Debug.Log("DOUBLE CLICK");
-    //}
 }
